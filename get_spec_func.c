33715014028df7f6f34e139dd580f79602012567
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-02 12:25:05 -0700

    function that checks for specifier, and points to proper function

--- get_spec_func.c
+++ get_spec_func.c
@@ -1,3 +1,38 @@
 #include "printf.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int 
+int (*get_spec_func(char *str))(va_list list)
+{
+	format specs[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_dec},
+		{"i", print_int},
+	};
+
+	*i = *i + 1;
+
+	if (str[*i] == '\0')
+	{
+		return (-1)
+	}
+	if (str[*i] == '%')
+	{
+		_putchar('%');
+		return (1);
+	}
+
+	number_formats = sizeof(formats) / sizeof(formats[0]);
+	for (size = j = 0; j < number_formats; j++)
+	{
+		if (str[*i] == formats[j].type)
+		{
+			size = formats[j].f(list);
+			return (size);
+		}
+	}
+	_putchar('%'), _putchar(str[*i]);
+
+	return (2);
+}
