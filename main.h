9c95904d21d52542f66ab83b8388816f0c7358d8
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-02 12:46:58 -0700

    changed name

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN
+#define MAIN
+
+typedef struct _format
+{
+	char *type
+	int (*func)(va_list list);
+} format;
+
+int _printf(const char *format, ...);
+
+int _putchar(char c);
+
+int (*get_spec_func(char *str))(va_list list);
+
+int print(const char *str, va_list list);
+
+int print_char(va_list list);
+
+int print_string(va_list list);
+
+#endif
