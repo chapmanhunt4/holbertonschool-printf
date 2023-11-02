93152f5ffb63c6145dba681b9c376f464692942b
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-02 12:29:47 -0700

    created putchar and strlen functions to call

---
+++ utils.c
@@ -0,0 +1,24 @@
+#include "printf.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - prints char to stdout
+ * @c: char to be printed
+ *
+ * Return: character
+ **/
+
+int _putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int _strlen(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		return (i);
+	}
