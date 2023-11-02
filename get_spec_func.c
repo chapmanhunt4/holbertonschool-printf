ee1328fbb2cd6258e5ff14d45d5dd898d26b1fa8
jacobleon2117 <jacobleon2117@gmail.com>
2023-11-02 13:13:36 -0500

    adding getspecfunc

--- get_spec_func.c
+++ get_spec_func.c
@@ -1,3 +1,12 @@
 #include "printf.h"
+#include <stdlib.h>
+#include <stdio.h.
 
-int 
+int (*get_spec_func(char *f)(va_list list)
+{
+	format spec[] = {
+	{"c", print_char},
+	{"s", print_string},
+	{"%", print_arg},
+	{"
+}"  
