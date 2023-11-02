0b3128aa43f2d0ab9d23afe73bd3dfc3fb701f4a
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-02 12:39:00 -0700

    added remaining prototypes

--- printf.h
+++ printf.h
@@ -3,12 +3,20 @@
 
 typedef struct _format
 {
-	char *form
-	int (*f)(va_list list);
+	char *type
+	int (*func)(va_list list);
 } format;
 
 int _printf(const char *format, ...);
 
+int _putchar(char c);
 
+int (*get_spec_func(char *str))(va_list list);
+
+int print(const char *str, va_list list);
+
+int print_char(va_list list);
+
+int print_string(va_list list);
 
 #endif
