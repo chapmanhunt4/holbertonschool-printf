6bb4324e2bf666ed6cb9788a576ae7b7d70574ff
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-06 17:03:05 -0800

    put back in spec_func

--- spec_func.c
+++ spec_func.c
@@ -58,3 +58,43 @@ int print_string(va_list args)
 	return (size);
 	size++;
 }
+
+/**
+ * print_number - prints a number
+ * @args: va_list
+ **/
+
+int print_digit(va_list args)
+{
+        int number, i;
+        int size = 0;
+        char buffer[20];
+
+        number = va_arg(args, int);
+        if (number == INT_MIN)
+        {
+                _putchar('-');
+                number = -(number + 1);
+                size++;
+                buffer[size++] = '2';
+        }
+        else if (number < 0)
+        {
+                _putchar('-');
+                number = -number;
+                size++;
+        }
+
+        do
+        {
+                buffer[size++] = '0' + (number % 10);
+                number /= 10;
+        }
+        while (number > 0);
+
+        for (i = size - 1; i >= 0; i--)
+        {
+                _putchar(buffer[i]);
+        }
+        return (size);
+}
